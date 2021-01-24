943c01e52390d4291bc85efcc9d20c367ef1e790
Crowdbotics <team@crowdbotics.com>
2021-01-24 16:13:53 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Feeee5
+
+admin.site.register(Feeee5)
 
 # Register your models here.
--- backend/home/api/v1/viewsets.py
+++ backend/home/api/v1/viewsets.py
@@ -1,3 +1,6 @@
+from rest_framework import viewsets
+from rest_framework import authentication
+from .serializers import CustomTextSerializer, Feeee5Serializer, HomePageSerializer
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.permissions import IsAdminUser
@@ -11,7 +14,7 @@ from home.api.v1.serializers import (
     HomePageSerializer,
     UserSerializer,
 )
-from home.models import CustomText, HomePage
+from home.models import CustomText, Feeee5, HomePage
 
 
 class SignupViewSet(ModelViewSet):
@@ -49,3 +52,12 @@ class HomePageViewSet(ModelViewSet):
     authentication_classes = (SessionAuthentication, TokenAuthentication)
     permission_classes = [IsAdminUser]
     http_method_names = ["get", "put", "patch"]
+
+
+class Feeee5ViewSet(viewsets.ModelViewSet):
+    serializer_class = Feeee5Serializer
+    authentication_classes = (
+        authentication.SessionAuthentication,
+        authentication.TokenAuthentication,
+    )
+    queryset = Feeee5.objects.all()
